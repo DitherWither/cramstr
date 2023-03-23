34c879dcd21fb5603bb0a0d6977bd304a3579779
DitherWither <demonslayervardhan@gmail.com>
2023-03-23 22:19:08 +0530

    Create a basic FlashCardSet Implementation
    The implementation hasn't been tested, but it is pretty late, so I'm letting this stay untested for now

---
+++ Shared/FlashCard.cs
@@ -0,0 +1,44 @@
+namespace Shared;
+
+/// <summary>
+/// A single flashcard. Stores the question and the answer for now.
+/// </summary>
+public class FlashCard
+{
+    /// <summary>
+    ///     The question, fairly self explanatory.
+    /// </summary>
+    ///
+    /// <remarks>
+    ///     
+    ///     Should be displayed first.
+    ///     Should default to an empty string
+    /// </remarks>
+    public string Question { get; set; } = "";
+
+    /// <summary>
+    ///     Answer to the question, should also have a short explanation
+    /// </summary>
+    ///
+    /// <remarks>
+    ///     Should only be displayed after clicking a button or some other user input
+    /// </remarks>
+    public string Answer { get; set; } = "";
+
+    /// <summary>
+    ///     Stores how frequently the user got the answer right.
+    ///     Incremented when answered correctly, and Decremented when not.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         Questions with a lower correctness score should be displayed first.
+    ///     </para>
+    ///     <para>
+    ///         This way, Questions that are frequently answered wrong will be memorised faster.
+    ///     </para>
+    ///     <para>
+    ///         Might be ignored, depending upon the user preferences.
+    ///     </para>
+    /// </remarks>
+    public int CorrectnessScore { get; set; }
+}
