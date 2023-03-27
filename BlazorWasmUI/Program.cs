9d82dd98fc9ef695e25605da85cad1c59d348782
DitherWither <demonslayervardhan@gmail.com>
2023-03-27 16:50:30 +0530

    Move to using Blazorize Bulma and add a basic view page

--- BlazorWasmUI/Program.cs
+++ BlazorWasmUI/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bulma;
+using Blazorise.Icons.FontAwesome;
 using BlazorWasmUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,5 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
     { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddBlazorise(options => { options.Immediate = true; })
+    .AddBulmaProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
